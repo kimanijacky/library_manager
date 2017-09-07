0e6e6dd5068d713efd179fb0751164d8aaae38b1
andela-jkimani <jackline.kimani@andela.com>
2017-09-07 13:05:03 +0300

    Create seed data

---
+++ server/seeds/seed.js
@@ -0,0 +1,29 @@
+module.exports = {
+  seeds: [
+    {
+      name: "Harry Potter",
+      available: true,
+      count: 0
+    },
+    {
+      name: "The Little Mermaid",
+      available: true,
+      count: 0
+    },
+    {
+      name: "Programming for Beginners",
+      available: true,
+      count: 0
+    },
+    {
+      name: "Lion King",
+      available: true,
+      count: 0
+    },
+    {
+      name: "Gone Girl",
+      available: true,
+      count: 0
+    }
+  ]
+}
