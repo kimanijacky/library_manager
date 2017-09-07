769cd7b33c145b88658a6e1e54ee49d68f0d9eba
andela-jkimani <jackline.kimani@andela.com>
2017-09-07 14:34:29 +0300

    Display all books on database

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import request from 'superagent'
+
+const style = {
+  width: '700px',
+  margin: '20px 50px',
+  padding: '20px',
+  backgroundColor: 'lightblue',
+  fontSize: '20px',
+  border: '1px solid black',
+  borderRadius: '20px'
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      name: '',
+      description: ''
     }
+  }
+
+  componentWillMount() {
+    this.fetchBooks();
+  }
+
+  fetchBooks() {
+    request
+      .get('http://localhost:3000/books')
+      .then((res) => {
+        this.setState({
+          books: res.body
+        })
+      });
   }
 
   render() {
     return (
       <div>
-        My books
+        <div>
+          {this.state.books.map((book, key) => {
+            return (
+              <div key={book._id} style={{display: 'flex', justifyContent: 'center'}}>
+                <p style={style}>{book.name}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin: *");
+    res.header("Access-Control-Allow-Credentials: true ");
+    res.header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+    res.header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+    next();
+});
+
 mongoose.connect('mongodb://localhost:27017/library', (err) => {
   err ? console.log(err) : console.log('DB connected!!')
 });
