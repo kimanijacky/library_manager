470ffd8c58150f60f4672d2210b1df14ffc50815
andela-jkimani <jackline.kimani@andela.com>
2017-09-07 13:02:03 +0300

    Set up and connect to database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const seed = require('./server/seeds/seed.js');
 
 const app = express();
 
@@ -10,9 +13,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+mongoose.connect('mongodb://localhost:27017/library', (err) => {
+  err ? console.log(err) : console.log('DB connected!!')
+});
+
+mongoose.connection.on('connected', () => {
+  Books.remove({}, (err) => {
+    err ? console.log(err) : console.log('Collection removed');
+  });
+
+  Books.create(seed.books, () => {
+    err ? console.log(err) : console.log('Collection created');
+  });
+});
+
 app.get('/', (req, res) => {
   console.log('App working');
-})
+});
 
 app.listen(port, (err) => {
   err ? err : console.log('Connection successful at port ', port);
